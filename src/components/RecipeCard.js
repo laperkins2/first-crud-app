7cc2ba9d087cc00561673dcf31199b0eaec6d36f
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-06-16 14:01:09 -0500

    updated to add delete in detailed

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -5,6 +5,7 @@ export default function RecipeCard({
   recipeTitle,
   ingredients,
   author,
+  onDelete,
 }) {
   return (
     <div className="w-100 p-4 m-4 card border-4 rounded-2xl overflow-hidden shadow-lg bg-yellow-200">
@@ -16,7 +17,14 @@ export default function RecipeCard({
       <br></br>
       Author: {author}
       <br></br>
-      {onDelete && <button onClick={onDelete}>Delete</button>}
+      {onDelete && (
+        <button
+          onClick={onDelete}
+          className="px-3 py-1 mt-2 bg-red-500 text-white rounded-md"
+        >
+          Delete
+        </button>
+      )}
     </div>
   );
 }
