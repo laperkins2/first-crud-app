4b83165302cf7c50277cfad78b5216d44ff30ca0
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-07-08 19:56:33 -0500

    commentted out unused things

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { auth } from 'firebase.config';
+// import { auth } from 'firebase.config';
 import { registerUser, login } from '@/utils/authUtils';
 
 const RegistrationForm = () => {
